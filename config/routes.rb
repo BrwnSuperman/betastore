735e04229c18eb01f4f32073f3bda085e80119a6
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-03 21:12:33 -0400

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Betastore::Application.routes.draw do
+  root :to => 'products#index'
   get '/log_in'  => 'log_ins#new', as: 'log_in'
   post '/log_in'  => 'log_ins#create'
   post '/log_out' => 'log_ins#destroy', as: 'log_out'
   resources :products, :subscriptions
-  root :to => 'products#index'
+
 
 end
