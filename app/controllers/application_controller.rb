f5d5068738d2ffc6c0828743eb2a50036f43d3f6
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-03 20:34:21 -0400

    Log In Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
   before_filter :require_log_in
 
   protected
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    false
+    cookies[:email].present?
   end
 end
