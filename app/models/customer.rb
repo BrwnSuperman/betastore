1a970ccdc5ea1a85862ff0e855e6c93887ff20f3
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-08 19:48:22 -0400

    Update

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,3 @@
 class Customer < ActiveRecord::Base
-  has_many :orders
-  has_many :line_items, :through => :orders
-  has_many :products, :through => :line_items
-
-  validates_presence_of :name, :email, :password
+  has_secure_password
 end
