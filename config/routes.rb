a9dfbb383a2120d46223fe4c0abde08a86dbf337
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-08 18:21:45 -0400

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Betastore::Application.routes.draw do
 
-  resources :products, only: [:index] do
-    resources :orders, only: [:new, :create, :update, :delete, :index] do
-      resources :refunds, only [:index, :new, :create]
-    end
-  end
+  resources :products, only: [:index]
+  resources :orders, only: [:new, :create, :update, :delete, :index]
+  resources :refunds, only: [:index, :new, :create]
   resources :subscriptions
   root :to => 'products#index'
   get '/log_in'  => 'log_ins#new', as: 'log_in'
   post '/log_in'  => 'log_ins#create'
   post '/log_out' => 'log_ins#destroy', as: 'log_out'
-
 end
