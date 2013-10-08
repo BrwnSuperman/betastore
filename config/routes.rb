0a7e78852d1b98ec4776e88a29bb43b92166d549
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-08 18:51:53 -0400

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Betastore::Application.routes.draw do
 
-  resources :products, only: [:index]
-  resources :orders, only: [:new, :create, :update, :delete, :index]
-  resources :refunds, only: [:index, :new, :create]
-  resources :subscriptions
-  root :to => 'products#index'
-  get '/log_in'  => 'log_ins#new', as: 'log_in'
-  post '/log_in'  => 'log_ins#create'
+  resources :products, :orders
+  resources :customers, only: [:new, :create]
+    get '/sign_up' => 'customers#new', as: 'sign_up'
+    post '/sign_up' => 'customers#create'
+  resources :subscriptions, only:[:create, :new, :show]
+
+  resources :orders do
+    resources :refunds
+  end
+  get '/log_in' => 'log_ins#new', as: 'log_in'
+  post '/log_in' => 'log_ins#create'
   post '/log_out' => 'log_ins#destroy', as: 'log_out'
+
+  root :to => 'products#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008205526) do
+ActiveRecord::Schema.define(version: 20131008224331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20131008205526) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "zip_code"
+    t.string   "password"
   end
 
   create_table "line_items", force: true do |t|
