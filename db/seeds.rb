25a67d53dada58247081c5086b09dfc6eddc0a66
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-10 21:00:30 -0400

    Products displayed!

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,19 @@ hoodie = Product.create(name: 'Hoodie', price: 3.99, url: 'http://pjb3.github.io
 journal = Product.create(name: 'Journal', price: 3.99, url: 'http://pjb3.github.io/betastore/products/journal.jpg')
 sticker = Product.create(name: 'Sticker', price: 3.99, url: 'http://pjb3.github.io/betastore/products/sticker.jpg')
 hat = Product.create(name: 'Hat', price: 3.99, url: 'http://pjb3.github.io/betastore/products/hat.jpg')
+
+tanveer = Customer.create(name: 'Tanveer', email: 'test@example.com')
+me = Customer.create(name: 'Paul Barry', email: 'mail@paulbarry.com')
+test = Customer.create(name: 'John Doe', email: 'test@example.com')
+Customer.create(name: 'Nowhere Man', email: 'man@nowhere.com')
+
+order = Order.create(customer: tanveer, placed_at: '2013-09-27')
+LineItem.create(order: order, quantity: 1, product: iphone_case)
+LineItem.create(order: order, quantity: 2, product: hoodie)
+
+order = Order.create(customer: me, placed_at: Time.current)
+LineItem.create(order: order, quantity: 3, product: shirt)
+
+order = Order.create(customer: test, placed_at: Time.current)
+LineItem.create(order: order, quantity: 4, product: journal)
+LineItem.create(order: order, quantity: 5, product: sticker)
