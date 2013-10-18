7184aa4c80f4e15b70f6e324387edb0bfded4d07
BrwnSuperman <huq.tanveer@gmail.com>
2013-10-17 21:16:46 -0400

    Test changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,40 @@ class ProductsController < ApplicationController
   skip_before_filter :require_log_in
 
   def index
+    @products = Product.all
     respond_to do |format|
-      format.html { @products = Product.all}
-      format.any(:rss) { @products = Product.all }
+      format.html
+      format.rss
+      format.json do
+        render json: @products
+      end
     end
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html do
+          redirect_to product_path(@product), notice: 'Product was saved;'
+        end
+        format.json do
+          render json: @product
+        end
+      else
+        format.html do
+          render 'edit'
+        end
+        format.json do
+          render json: { errors: @product.errors }, status:422
+        end
+      end
+    end
+  end
+  private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 
 end
